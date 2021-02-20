1894e9fbee6ac282e902cc947273b8e57b098e13
Stephen Reynolds <sgrjr@deliverance.me>
2021-02-20 06:28:40 -0500

    not sure

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {   
         $schedule->call(function () {
-            file_put_contents('schedule.txt','running scheduler \n', FILE_APPEND);
+           file_put_contents('schedule.txt',"running scheduler ". Carbon::now() . "\n", FILE_APPEND);
         })->everyMinute();
+
         $schedule->command('twicedaily:update')->twiceDaily(1,13);
 
         $schedule->call(function () {
